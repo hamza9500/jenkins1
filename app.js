40c7270c83c3fa84ef8b0b03bdc0adba390ed779
hamza <hamza@hotmail.com>
2018-04-30 14:41:00 +0300

    edit4

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-	  res.send('system admin cloudview');
+	  res.send('sys admin cloudview');
 });
  
 app.listen(process.env.PORT || 5000);
